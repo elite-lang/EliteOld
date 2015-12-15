d688abfbaab05334bb83f3c7edc3179909a68784
Norgerman <xyn0410@gmail.com>
2015-12-15 23:14:23 +0800

    heheda

--- scripts/fix_ubuntu_llvm.sh
+++ scripts/fix_ubuntu_llvm.sh
@@ -5,4 +5,4 @@
 # @Last Modified time: 2015-12-15 22:36:13
 
 
-sudo cp -f ./scripts/fix_ubuntu/LLVMConfig.cmake /usr/share/llvm-3.6/cmake/LLVMConfig.cmake
+cp -f ./scripts/fix_ubuntu/LLVMConfig.cmake /usr/share/llvm-3.6/cmake/LLVMConfig.cmake
