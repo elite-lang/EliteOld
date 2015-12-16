674bb1d8f7e7f385c0b8aa9b854ae8c9d0542ee6
sxf <sun_xfancy@163.com>
2015-12-16 22:47:30 +0800

    v0.6.5
    重要底层模块bug修复
    增加对字符串的转义操作
    更换新版的底层meta数据结构
    ExIconv更改编码结构, 内置字符串编码为UTF-16LE
    
    Signed-off-by: sxf <sun_xfancy@163.com>

--- ExIconv/test/EstringTest.cpp
+++ ExIconv/test/EstringTest.cpp
@@ -2,7 +2,7 @@
 * @Author: sxf
 * @Date:   2015-12-16 12:02:18
 * @Last Modified by:   sxf
-* @Last Modified time: 2015-12-16 18:14:37
+* @Last Modified time: 2015-12-16 20:39:39
 */
 
 #include <gtest/gtest.h>
@@ -34,4 +34,13 @@ __ESTRING_TEST__(ToUtf8)
 	estring str("我的中国心");
 	string hello("我的中国心");
 	EXPECT_EQ(str.to_utf8() == hello, true);
+}
+
+__ESTRING_TEST__(Select)
+{
+	estring str("hello您好, 这是一段中英混排");
+	for (int i = 0; i < 8; ++i)
+		printf("%04X ", str[i]);
+	printf("\n");
+	EXPECT_EQ(str[4], 'o');
 }
