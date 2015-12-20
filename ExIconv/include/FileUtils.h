770e08d9be3fe636efc251834654cffad6199a2e
sxf <sun_xfancy@163.com>
2015-12-20 11:46:05 +0800

    添加了一个文件夹遍历
    Signed-off-by: sxf <sun_xfancy@163.com>

--- ExIconv/include/FileUtils.h
+++ ExIconv/include/FileUtils.h
@@ -2,7 +2,7 @@
 * @Author: sxf
 * @Date:   2015-12-11 18:42:32
 * @Last Modified by:   sxf
-* @Last Modified time: 2015-12-17 20:34:59
+* @Last Modified time: 2015-12-20 11:43:19
 */
 
 
@@ -11,6 +11,19 @@
 
 #include <string>
 
+/**
+ * @brief 文件遍历的接口
+ */
+class IFileTraversal {
+public:
+	virtual void Work(const std::string& now_path, 
+			  const std::string& filename, 
+			  const std::string& suffix) = 0;
+};
+
+/**
+ * @brief 跨平台的文件系统接口
+ */
 class FileUtils
 {
 public:
@@ -18,6 +31,8 @@ public:
 	static bool test_dir(const std::string& path);
 	static bool test_file(const std::string& path);
 	static std::string get_current_path();
+	static int dir_traversal(const std::string& path, IFileTraversal& ifile);
+	static int dir_recursive_traversal(const std::string& path, IFileTraversal& ifile);
 };
 
 
---
+++ ExIconv/include/PathUtils.h
@@ -0,0 +1,24 @@
+/* 
+* @Author: sxf
+* @Date:   2015-12-20 11:38:17
+* @Last Modified by:   sxf
+* @Last Modified time: 2015-12-20 11:38:51
+*/
+
+
+#ifndef PATH_UTILS_H
+#define PATH_UTILS_H
+
+/**
+ * @brief 跨平台的路径拼接工具类
+ */
+class PathUtils
+{
+public:
+
+
+	
+};
+
+
+#endif // PATH_UTILS_H
