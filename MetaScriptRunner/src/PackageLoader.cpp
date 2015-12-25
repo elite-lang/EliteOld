01ef47f4584d8abe4f60db9ea31fd3deace6a281
sxf <sun_xfancy@163.com>
2015-12-25 10:44:01 +0800

    添加部分doc 添加packages搜索
    Signed-off-by: sxf <sun_xfancy@163.com>

--- MetaScriptRunner/src/PackageLoader.cpp
+++ MetaScriptRunner/src/PackageLoader.cpp
@@ -2,13 +2,27 @@
 * @Author: sxf
 * @Date:   2015-12-24 17:21:09
 * @Last Modified by:   sxf
-* @Last Modified time: 2015-12-24 17:21:46
+* @Last Modified time: 2015-12-25 10:43:00
 */
 
 #include "PackageLoader.h"
+#include "FileUtils.h"
+#include <iostream>
+
+class DirFileTraversal : public IFileTraversal {
+public:
+	virtual void Work(const std::string& now_path, 
+			  const std::string& filename, 
+			  const std::string& suffix) 
+	{
+		cout << now_path + "/" + filename + "." + suffix << endl;
+	}
+};
+
+
 
 PackageLoader::PackageLoader(const string& base_path) {
-
+	this->base_path = base_path;
 }
 
 PackageLoader::~PackageLoader() {
@@ -20,7 +34,8 @@ PackageLoader::~PackageLoader() {
  * @brief 找出目录下所以的可加载项
  */	
 void PackageLoader::FindAll() {
-
+	DirFileTraversal dft;
+	FileUtils::dir_recursive_traversal(base_path, dft);
 }
 
 
