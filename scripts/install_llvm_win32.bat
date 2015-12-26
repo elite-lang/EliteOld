8319e0fd3e1a668f25094fed273a27de47b41a4d
sxf <sun_xfancy@163.com>
2015-12-26 22:58:36 +0800

    fix
    Signed-off-by: sxf <sun_xfancy@163.com>

--- scripts/install_llvm_win32.bat
+++ scripts/install_llvm_win32.bat
@@ -2,6 +2,7 @@ git clone --depth=1 -b release_36 https://github.com/llvm-mirror/llvm.git
 cd llvm
 mkdir build
 cd build
-cmake -Dcmake_install_prefix=../../LLVM_INSTALL ..
+call "%VS130COMNTOOLS%vsvars32.bat" x86
+cmake -DCMAKE_INSTALL_PREFIX="C:/projects/elite/LLVM_SDK" ..
 devenv "llvm.sln" /build release /project "install.vcxproj"
 cd ../..
